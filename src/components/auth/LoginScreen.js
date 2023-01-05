06d3872532a5d8c3c72f2ce3cb52049aeb267e8e
erc83 <espinoza.eric@gmail.com>
2023-01-05 15:53:44 -0300

    LoginScreen con sass ok

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,8 +1,65 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const LoginScreen = () => {
   return (
-    <div>LoginScreen</div>
+    <>
+      <h3 className="auth__title">Login</h3>
+
+      <form>
+
+        <input
+          className="auth__input" 
+          type="text"
+          placeholder="Email"
+          name="email"
+          autoComplete='off'
+        />
+
+        <input
+          className="auth__input" 
+          type="password"
+          placeholder="Password"
+          name="password"
+        />
+
+        <button
+          type="submit"
+          className="btn btn-primary btn-block"
+          /* disabled={ true } */
+        >
+          Login
+        </button>
+
+        <div className='auth__social_networks'>
+          <p>Login with social networks</p>
+          
+          <hr />
+
+          <div className="google-btn">
+              <div className="google-icon-wrapper">
+                  <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="google button" />
+              </div>
+              <p className="btn-text">
+                  <b>Sign in with google</b>
+              </p>
+          </div>
+        </div>
+        
+        <Link
+          to="/auth/register"
+        >
+          Create new account
+        </Link>
+
+
+
+      </form>
+
+
+
+    
+    </>
   )
 }
 
