5c3506e625291ce1dfc33f991744fb421eef55a4
erc83 <espinoza.eric@gmail.com>
2023-01-11 12:05:35 -0300

    handleGoogleLogin ok

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -3,7 +3,7 @@ import { useDispatch } from "react-redux";
 
 import { Link } from 'react-router-dom'
 import { useForm } from '../../hooks/useForm'
-import { startLoginEmailPassword } from '../actions/auth';
+import { startGoogleLogin, startLoginEmailPassword } from '../actions/auth';
 
 const LoginScreen = () => {
 
@@ -21,6 +21,11 @@ const LoginScreen = () => {
     e.preventDefault()
     // console.log(email, password)
     dispatch( startLoginEmailPassword( email, password ) )
+  }
+
+
+  const handleGoogleLogin = () => {
+    dispatch( startGoogleLogin() );
   }
 
 
@@ -64,7 +69,10 @@ const LoginScreen = () => {
           
           <hr />
 
-          <div className="google-btn">
+          <div 
+            className="google-btn"
+            onClick={ handleGoogleLogin }
+          >
               <div className="google-icon-wrapper">
                   <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="google button" />
               </div>
