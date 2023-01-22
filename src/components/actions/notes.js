2653a49b2b887223bdcabe5f52f65bfb110edc6e
erc83 <espinoza.eric@gmail.com>
2023-01-22 19:11:41 -0300

    side panel update and sweetAlert2 message

--- src/components/actions/notes.js
+++ src/components/actions/notes.js
@@ -1,3 +1,5 @@
+import Swal from 'sweetalert2';
+
 import { db } from '../../firebase/firebaseConfig';
 import { collection, addDoc, updateDoc, doc } from 'firebase/firestore'
 import { loadNotes } from "../helpers/loadNotes";
@@ -8,9 +10,7 @@ import { types } from '../types/types'
 export const startNewNote = () => {
     return async (dispatch, getState) => {    // segundo argumento para obtener el state
         
-        // const state = getState();
         const { uid } = getState().auth;    
-        //console.log( uid );
     
         const newNote = {
             title: '',
@@ -19,7 +19,6 @@ export const startNewNote = () => {
         }
 
         const doc = await addDoc(collection(db, `${ uid }/journal/notes`), newNote )     
-        // console.log(doc)
         dispatch( activeNote( doc.id, newNote ))  // hacemos el dispatch al reducer
     }
 }
@@ -55,21 +54,27 @@ export const startSaveNote = ( note ) => {
 
         const { uid } = getState().auth;
 
-        // aqui se borra la propiedad de la url que esta undefine aún antes de la extracción { ...note }
         if( !note.url ) {
             delete note.url;
         }
 
-        // verificar que si quiero grabar algo que tiene null firebase no deja que se grabe
-        // lo que hay que eliminar el objeto es el id que ya esta en firestore
         const noteToFirestore = { ...note }; // operador spread para separar toda la nota
         delete noteToFirestore.id           
-        // console.log(noteToFirestore)    // para ver el objeto con las propiedades que tiene
 
-
-        // await db.doc(`${ uid }/journal/notes/${ note.id }`).update( noteToFirestore )    // se tiene que utilizar el id de la nota forma de guardar en la version 8
-        await updateDoc(doc(db, `${ uid }/journal/notes`, note.id ), noteToFirestore )         // forma de guardar en la version 9 
+        await updateDoc(doc(db, `${ uid }/journal/notes`, note.id ), noteToFirestore )       
         
+        dispatch( refreshNote( note.id, noteToFirestore ) );   // para cambiar solo la nota que cambia
         
+        Swal.fire('Saved', note.title, 'success');
     }   
 }
+
+export const refreshNote = ( id, note ) => ({
+    type: types.notesUpdate,
+    payload: {
+        id,
+        note: {
+            id, ...note     
+        }
+    }
+})
